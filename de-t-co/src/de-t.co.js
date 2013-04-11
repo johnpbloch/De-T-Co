84e2262f9461c973d6c794d8368a86cc57eb6687
John P. Bloch <john.bloch@10up.com>
2013-04-11 10:37:08 -0400

    Restructure javascript

--- de-t-co/src/de-t.co.js
+++ de-t-co/src/de-t.co.js
@@ -1,16 +1,16 @@
 (function(window, undefined){
-	var document = window.document,
-		clean = function(){
-			var links = document.querySelectorAll('a[href^="http://t.co/"], a[href^="https://t.co/"]'),
-				x,
-				interval;
+	var document = window.document;
+	function clean() {
+		var links = document.querySelectorAll('a[href^="http://t.co/"], a[href^="https://t.co/"]'),
+			x,
+			interval;
 
-			for( x = 0; x < links.length; x += 1 ) {
-				if ( undefined !== links[x].dataset && undefined !== links[x].dataset.expandedUrl ) {
-					links[x].href = links[x].dataset.expandedUrl;
-				}
+		for( x = 0; x < links.length; x += 1 ) {
+			if ( undefined !== links[x].dataset && undefined !== links[x].dataset.expandedUrl ) {
+				links[x].href = links[x].dataset.expandedUrl;
 			}
-		};
-	
+		}
+	}
+
 	interval = window.setInterval( clean, 1000 );
 }(this));
