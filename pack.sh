a152dd57ed2af82b9dc9a3ad3b91e15fd1b45c5c
John P. Bloch <john.bloch@10up.com>
2014-06-13 09:58:47 -0600

    Add command for creating zip of extension

--- pack.sh
+++ pack.sh
@@ -7,3 +7,6 @@ mkdir -p de-t-co/assets
 convert de-t-co.svg -flatten -resize 128x128 de-t-co/assets/icon128.png
 convert de-t-co.svg -flatten -resize 48x48 de-t-co/assets/icon48.png
 convert de-t-co.svg -flatten -resize 16x16 de-t-co/assets/icon16.png
+
+cd de-t-co
+zip ../de-t-co.zip src/* assets/* manifest.json
