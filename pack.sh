91e597a5696633d75c3864ce89932d281e7d6205
John P. Bloch <john.bloch@10up.com>
2014-06-13 09:47:38 -0600

    Add a script to create assets and ignore them

---
+++ pack.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -r de-t-co/assets
+
+mkdir -p de-t-co/assets
+
+convert de-t-co.svg -flatten -resize 128x128 de-t-co/assets/icon128.png
+convert de-t-co.svg -flatten -resize 48x48 de-t-co/assets/icon48.png
+convert de-t-co.svg -flatten -resize 16x16 de-t-co/assets/icon16.png
