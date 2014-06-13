448e66237ad9b92be073e4dcc1a67bcee9d039d7
John P. Bloch <john.bloch@10up.com>
2014-06-13 10:01:49 -0600

    Clean the zip out first

--- pack.sh
+++ pack.sh
@@ -8,5 +8,6 @@ convert de-t-co.svg -flatten -resize 128x128 de-t-co/assets/icon128.png
 convert de-t-co.svg -flatten -resize 48x48 de-t-co/assets/icon48.png
 convert de-t-co.svg -flatten -resize 16x16 de-t-co/assets/icon16.png
 
+rm de-t-co.zip
 cd de-t-co
 zip ../de-t-co.zip src/* assets/* manifest.json
