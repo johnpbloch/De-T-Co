91f52603e9711eb3312416b66459ac11172cd0e5
John P Bloch <johnpbloch@gmail.com>
2015-06-22 23:53:59 -0500

    Add firefox add-on code

---
+++ firefox/data/de-t.co.js
@@ -0,0 +1,21 @@
+(function (window, undefined) {
+	var document = window.document,
+			interval;
+
+	function clean() {
+		var links = document.querySelectorAll('a[href^="http://t.co/"], a[href^="https://t.co/"]'),
+				x;
+
+		for (x = 0; x < links.length; x += 1) {
+			if (undefined !== links[x].dataset) {
+				if (undefined !== links[x].dataset.expandedUrl) {
+					links[x].href = links[x].dataset.expandedUrl;
+				} else if (undefined !== links[x].dataset.fullUrl) {
+					links[x].href = links[x].dataset.fullUrl;
+				}
+			}
+		}
+	}
+
+	interval = window.setInterval(clean, 1000);
+}(window));
